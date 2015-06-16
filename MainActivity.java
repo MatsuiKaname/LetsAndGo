8a681eceae6f1643c2697005ad34e4ea9df471c0
kazuya12125@gmail.com <kazuya@mdl.cs.tsukuba.ac.jp>
2015-06-16 17:33:32 +0900

    first commit

---
+++ MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.kakizaki.recgnizeled;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private static final int REQUEST_CODE = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button button = (Button) findViewById(R.id.button);
+       //ToggleButton toggleButton1 = (ToggleButton)findViewById(R.id.toggle_button_1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                try {
+                    // インテント作成
+                    Intent intent = new Intent(
+                            RecognizerIntent.ACTION_RECOGNIZE_SPEECH); // ACTION_WEB_SEARCH
+                    intent.putExtra(
+                            RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                    intent.putExtra(
+                            RecognizerIntent.EXTRA_PROMPT,
+                            "VoiceRecognitionTest"); // お好きな文字に変更できます
+
+                    // インテント発行
+                    startActivityForResult(intent, REQUEST_CODE);
+                } catch (ActivityNotFoundException e) {
+                }
+            }
+        });
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // 自分が投げたインテントであれば応答する
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            String resultsString = "";
+
+            // 結果文字列リスト
+            ArrayList<String> results = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);
+
+            resultsString += results.get(0);
+
+            HttpGetTask task = new HttpGetTask(this);
+            if(resultsString.equals("走れ")){
+               // Toast.makeText(this, resultsString, Toast.LENGTH_LONG).show();
+                task.execute(0,1);
+
+            }
+            else if(resultsString.equals("止まれ")){
+                task.execute(0,0);
+            }
+            // トーストを使って認識文字を表示
+            Toast.makeText(this, resultsString, Toast.LENGTH_LONG).show();
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
